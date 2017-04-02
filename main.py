47b9764f73ac09450a45f7a9dd0f8dbc85772f09
Alidus <mainfaystain@gmail.com>
2017-04-02 21:05:06 +0500

    Changed IP

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import socket;
 
 print("Starting")
 serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-serv_socket.bind(('127.0.0.1', 53210))
+serv_socket.bind(('13.73.158.180', 53210))
 serv_socket.listen(10)
 
 print("Listening...")
 client_sock, client_addr = serv_socket.accept()
-#print(client_sock, client_addr)
+print(client_sock, client_addr)
 
 while True:
     data = client_sock.recv(1024)
